07f5b48724ba66289c4021110f3e2e29a1d3057a
Rajkumar-28 <70897549+Rajkumar-28@users.noreply.github.com>
2021-03-08 15:54:56 +0530

    Add files via upload

---
+++ Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrderList.Models
+{
+    public class Order
+    {
+        public int OrderId;
+        public int CustomerId;
+        public string FirtsName;
+        public string CustomerName;
+        public string ItemName;
+        public string ItemInformation;
+        public int CostOfItem;
+    }
+}
---
+++ customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrderList.Models
+{
+    public class customer
+    {
+        public int CustomerId;
+        public string FirtsName;
+        public string LastName;
+        public string Email;
+        public long PhoneNo;
+        public string Password;
+    }
+}
